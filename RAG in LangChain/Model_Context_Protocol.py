3f3f114957d4e7126cacb16fe20ecf0deb6cd40c
GedmantasC <gedmantas1997@gmail.com>
2026-02-27 12:52:34 +0200

    Add Model_Context_Protocol.py

--- RAG in LangChain/Model_Context_Protocol.py
+++ RAG in LangChain/Model_Context_Protocol.py
@@ -6,3 +6,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+async def main():
+    # Connect to MCP server and fetch tools
+    client = MultiServerMCPClient({
+        "weather": {
+            "url": "http://localhost:8000/mcp/",
+            "transport": "streamable_http",
+        }
+    })
+    tools = await client.get_tools()
+    
