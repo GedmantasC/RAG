6b97b1649cf039d023ed1a8e9e9a0df5078f0c2d
GedmantasC <gedmantas1997@gmail.com>
2026-03-02 10:39:25 +0200

    Add Model_Context_Protocol.py

---
+++ RAG in LangChain/S2P4_experiment_function_calling.py
@@ -0,0 +1,36 @@
+from langchain.agents import create_agent
+from langchain_core.tools import tool
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain_openai import ChatOpenAI
+import os
+import toml
+from openai import OpenAI
+import getpass
+from langgraph.checkpoint.sqlite import SqliteSaver
+from mcp.server.fastmcp import FastMCP
+
+# Load API key
+secrets = toml.load(".key/secrets.toml")
+os.environ["OPENAI_API_KEY"] = secrets["OPENAI_API_KEY"]
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+client = OpenAI(api_key=secrets["OPENAI_API_KEY"])
+
+# Define tools with the calculator_add function
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "calculator_add",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "num1": {"type": "number"},
+                    "num2": {"type": "number"},
+                },
+                "required": ["num1", "num2"],
+            },
+        },
+    }
+]
