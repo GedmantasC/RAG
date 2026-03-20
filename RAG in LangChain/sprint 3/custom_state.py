72790c3f7ae866bd126f5f7737b5d43a5efabd86
GedmantasC <gedmantas1997@gmail.com>
2026-03-20 18:56:02 +0200

    Add custom_state.py

--- RAG in LangChain/sprint 3/custom_state.py
+++ RAG in LangChain/sprint 3/custom_state.py
@@ -1,6 +1,8 @@
-from langchain.agents import create_agent, AgentState
+from langchain.agents import AgentState, create_agent
+from langchain.agents.middleware import AgentMiddleware
 
 # Define custom state by extending AgentState
+#The class provides structured memory for the agent.
 class ShoppingState(AgentState):
     cart_items: list[str]
     budget: float
@@ -10,7 +12,7 @@ class ShoppingState(AgentState):
 agent = create_agent(
     model="gpt-4o-mini",
     tools=[search_products, add_to_cart],
-    state_schema=ShoppingState
+    state_schema=ShoppingState #this tells how to store conversation
 )
 
 # Now you can initialize state when invoking
