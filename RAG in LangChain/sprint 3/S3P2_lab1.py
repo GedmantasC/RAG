21dd5089884ee841b9a2b1dcb109e87a2802ceb1
GedmantasC <gedmantas1997@gmail.com>
2026-03-26 15:10:50 +0200

    S3P2_lab1 update

--- RAG in LangChain/sprint 3/S3P2_lab1.py
+++ RAG in LangChain/sprint 3/S3P2_lab1.py
@@ -2,3 +2,8 @@ from langchain.agents import create_agent, AgentState
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langgraph.checkpoint.memory import MemorySaver
+
+# Step 1: Define custom state by extending AgentState
+class ShoppingState(AgentState):
+    cart_items: list[dict] = []  # List of items: [{"name": "Laptop", "price": 1299}, ...]
+    budget: float = 100.0         # User's budget
