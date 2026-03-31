52b92ebc5e72c82a51801c1da6bf07d51fa25879
GedmantasC <gedmantas1997@gmail.com>
2026-03-31 17:28:02 +0300

    Update of S3P3_lab1.py

--- RAG in LangChain/sprint 3/S3P2_lab1.py
+++ RAG in LangChain/sprint 3/S3P2_lab1.py
@@ -7,6 +7,8 @@ from langchain.agents import create_agent
 from langgraph.graph import MessagesState
 from langgraph.checkpoint.memory import MemorySaver
 from langchain.tools import ToolRuntime
+from langgraph.types import Command
+from langchain_core.messages import ToolMessage
 
 # Load API key
 secrets = toml.load(".key/secrets.toml")
