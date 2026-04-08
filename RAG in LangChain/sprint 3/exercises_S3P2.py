b84acc7e735050ddc3af039c2688e4fe1e1257b4
GedmantasC <gedmantas1997@gmail.com>
2026-04-08 17:51:58 +0300

    Update exercises_S3P2.py

--- RAG in LangChain/sprint 3/exercises_S3P2.py
+++ RAG in LangChain/sprint 3/exercises_S3P2.py
@@ -9,16 +9,25 @@ from langgraph.checkpoint.memory import MemorySaver
 from langchain.tools import ToolRuntime
 from langgraph.types import Command
 from langchain_core.messages import ToolMessage
+from langchain.agents import AgentState
+from typing import List, Dict, Any
 
 # Load API key
 secrets = toml.load(".key/secrets.toml")
 os.environ["OPENAI_API_KEY"] = secrets["OPENAI_API_KEY"]
 if not os.environ.get("OPENAI_API_KEY"):
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+  
 
-class TaskState (MessagesState):
+    
+
+
+class TaskState(AgentState):
     id: int  #task id
     title: str      #title of the task
     completed:bool  #completion status
+    pass
 
-    
+# Test your state schema
+print("TaskState defined!")
+print(f"Fields: {TaskState.__annotations__}")
