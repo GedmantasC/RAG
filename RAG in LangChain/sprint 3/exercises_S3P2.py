bd975212dfe2e90b0f7d478ef6df6dfa1deb9eaf
GedmantasC <gedmantas1997@gmail.com>
2026-04-08 17:27:39 +0300

    Update exercises_S3P2.py

--- RAG in LangChain/sprint 3/exercises_S3P2.py
+++ RAG in LangChain/sprint 3/exercises_S3P2.py
@@ -9,3 +9,9 @@ from langgraph.checkpoint.memory import MemorySaver
 from langchain.tools import ToolRuntime
 from langgraph.types import Command
 from langchain_core.messages import ToolMessage
+
+# Load API key
+secrets = toml.load(".key/secrets.toml")
+os.environ["OPENAI_API_KEY"] = secrets["OPENAI_API_KEY"]
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
